65e3a2e38c4659de68b7821c8442bcccf9cc6bc9
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2023-01-27 21:04:52 +0000

    edit navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import HeaderSocial from "./HeaderSocials"
-import TopNav from './top-nav'
+// import TopNav from './top-nav'
 import tuma from  '../../assets/tuma.png'
 import video from '../../assets/video1.mp4'
 
@@ -10,11 +10,13 @@ const header = () => {
     <section id="header">
       <header>
       <video id ="vida" src={video} autoPlay loop muted />
-        <TopNav />
+        {/* <TopNav /> */}
 
       <div className="container header_container">
         <h5>Hello, Welcome to</h5>
         <h1>Tumaini Ministries Centre</h1>
+        <h2><emp>You have been made clean already by the teaching I have given you.</emp></h2>
+        <h2>JOHN 15:3</h2>
         {/* <h3 className= "text-light">Needing Jesus Christ Together</h3> */}
         <img id ="logo" src={tuma} alt="" />
         <HeaderSocial />
